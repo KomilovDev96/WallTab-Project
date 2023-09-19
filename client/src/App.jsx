ede78e197f87d387fdec8afb0843c681b077e175
Azizbek1 <azizochilov569@gmail.com>
2023-09-19 19:22:33 +0500

    shriftlarni uladik

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { HeaderPNG, LogoSVG } from "./assets"
+import "./style/global.scss"
+const App = () => {
+
+  return (
+    <>
+      <h1>App</h1>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+// ======  SVG  ======
+import LogoSVG from './svg/logo.svg'
+
+
+//======  PNG   ======
+import HeaderPNG from './png/header.png'
+
+export {
+    LogoSVG,
+    HeaderPNG
+}

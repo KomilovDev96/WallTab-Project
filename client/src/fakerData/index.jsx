a17dbf45d7e9c941ab02ba53f3feac2a94ee99a4
Azizbek1 <azizochilov569@gmail.com>
2023-10-03 20:16:24 +0500

    asosiy sahifa header bo'limi
    Keyingi darsga biz gridlarni o'rganishimiz kerak !
    Uyga Vazifa : Man bergan maketni qilish kerak !

---
+++ client/src/fakerData/index.jsx
@@ -0,0 +1,27 @@
+import { AiOutlineInstagram, AiFillFacebook, AiFillTwitterCircle } from "react-icons/ai";
+import { BsTelegram, BsFillPersonFill } from "react-icons/bs";
+export const NavigateTopDATA = [
+    { id: 1, svg: <BsTelegram />, href: '/' },
+    { id: 2, svg: <AiOutlineInstagram />, href: '/' },
+    { id: 3, svg: <AiFillFacebook />, href: '/' },
+    { id: 4, svg: <AiFillTwitterCircle />, href: '/' },
+]
+export const NavigateBottomSVGDATA = [
+    { id: 1, icon: <BsFillPersonFill />, title: "Аккаунт", href: '/' },
+    { id: 2, icon: <BsFillPersonFill />, title: "КОРЗИНА", href: '/' },
+    { id: 3, icon: <BsFillPersonFill />, title: "СПОДОБАЛОСЬ", href: '/' },
+]
+
+export const NavigateBottomMENUDATA = [
+    { id: 1, title: "Главная", href: '/' },
+    { id: 2, title: "О нас", href: '/' },
+    { id: 3, title: "Блог", href: '/' },
+    { id: 4, title: "Контакти", href: '/' },
+    { id: 5, title: "Каталог", href: '/' },
+]
+
+export const NavigateBottomOFERDATA = [
+    { id: 1, title: "Оферта", href: '/' },
+    { id: 2, title: "Доставка", href: '/' },
+    { id: 3, title: "FAQ", href: '/' }
+]
--- client/src/layout/Header/NavigateTop/index.jsx
+++ client/src/layout/Header/NavigateTop/index.jsx
@@ -1,46 +1,105 @@
+import { NavigateBottomMENUDATA, NavigateBottomOFERDATA, NavigateBottomSVGDATA, NavigateTopDATA } from '../../../fakerData'
 import './style.scss'
 import { Col, Container, Row } from 'styled-bootstrap-grid'
+import { AiOutlineSearch } from "react-icons/ai";
+import { LogoSVG } from '../../../assets'
 const NavigateTop = () => {
     return (
         <nav className="navigate">
-            <Container>
-                <div className="navigate__top">
-                    <Row justifyContent="between">
-                        <Col md="6">
-                            <ul>
-                                <li>
-                                    <a href="#">
-                                        Google
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        Google
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        Google
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        Google
-                                    </a>
-                                </li>
+            <div className="navigate_top">
+                <Container>
+                    <Row justifyContent="between" alignItems="center">
+                        <Col md="4">
+                            <ul className="navigate_menuIcons">
+                                {NavigateTopDATA.map((item, index) => (
+                                    <li key={index}>
+                                        <a href={item.href}>
+                                            {item.svg}
+                                        </a>
+                                    </li>
+                                ))}
                             </ul>
                         </Col>
-                        <Col md="6">
-                            <div className="">
-                                <a href="+998 90 315 21 55">
+                        <Col md="4">
+                            <div className="navigate_phone">
+                                <a href="tel:+998 90 315 21 55">
                                     +998 90 315 21 55
                                 </a>
-                                <p>Натисніть, щоб скопіювати</p>
+                                <p>Нажмите, чтобы скопировать</p>
                             </div>
                         </Col>
                     </Row>
+                </Container>
+            </div>
+            <div className="navigate_bottom mainMenu">
+                <div className='mainMenu_top'>
+                    <Container>
+                        <Row>
+                            <Col lg="6">
+                                <div className="mainMenu_logo">
+                                    <a href="#">
+                                        <img src={LogoSVG} alt="svg" />
+                                    </a>
+                                    <form className="mainMenu_form">
+                                        <div className="mainMenu_form_item">
+                                            <input id='search' type="text" placeholder='Найти' />
+                                            <label htmlFor="search">
+                                                <AiOutlineSearch size={20} />
+                                            </label>
+                                        </div>
+                                    </form>
+                                </div>
+                            </Col>
+                            <Col lg="6" className="mainMenu_flex">
+                                <div className="mainMenu_sign">
+                                    <ul>
+                                        {NavigateBottomSVGDATA.map((item, index) => (
+                                            <li key={index}>
+                                                <a href={item.href}>
+                                                    {item.icon}
+                                                    <span className="mainMenu_sign_title">{item.title}</span>
+                                                </a>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
                 </div>
-            </Container>
+                <div className='mainMenu_bottom'>
+                    <Container>
+                        <Row>
+                            <Col lg="6">
+                                <ul>
+                                    {NavigateBottomMENUDATA.map((item, index) => (
+                                        <li key={index}>
+                                            <a href={item.href}>
+                                                {item.title}
+                                            </a>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </Col>
+                            <Col lg="6">
+                                <ul>
+                                    {NavigateBottomOFERDATA.map((item, index) => (
+                                        <li key={index}>
+                                            <a href={item.href}>
+                                                {item.title}
+                                            </a>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </Col>
+                        </Row>
+                    </Container>
+                    <ul>
+
+                    </ul>
+                </div>
+            </div>
+
         </nav>
     )
 }
